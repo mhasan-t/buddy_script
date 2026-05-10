0fe3fb3a36f4aea4d3f86e67f46bb882085ec02f
mhasan-t <muhibalhasan1@gmail.com>
2026-05-10 16:01:41 +0600

    registered models in admin site

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models.user import User
+from .models.post import Post
+from .models.comment import Comment
+from .models.reaction import Reaction
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Reaction)
+
